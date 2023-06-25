c395d24324d7eab2bbf0a943a5e902481d2b0af4
chhotu marandi <cmarandi29@gmail.com>
2023-06-25 13:20:06 +0530

    profile section

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,16 +1,24 @@
 import './Resume.css'
 
 function Resume ({name,email,mobile,address}) {
+
+
  return (
    <>
-   <h2 className='resume'>resume</h2>
+     <h2 className='resume'>resume</h2>
      <section className='profile-section'>
-       <div className='profile'>
-         <div className='profile-info'>
-           <h1>{name}</h1>
-           <p className='email'>Email-id : {email}</p>
-           <p className='mobile'>Mobile No : {mobile}</p>
-           <p className='address'>Address: {address}</p>
+       <div className='profile-info'>
+         <h1>{name}</h1>
+         <div className='email'>
+           <label>email-id : </label>
+           {email}
+         </div>
+         <div className='mobile'>
+           <label>mobile no :</label> {mobile}
+         </div>
+         <div className='email'>
+           <label>address : </label>
+           {address}
          </div>
        </div>
        <div className='profile-image'>
