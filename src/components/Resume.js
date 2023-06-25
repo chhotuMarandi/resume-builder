8d4d5e0bbbc73a478b663d3fc014df19d3620274
chhotu marandi <cmarandi29@gmail.com>
2023-06-25 12:53:49 +0530

    added image

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,20 +1,25 @@
+import './Resume.css'
+
 function Resume ({name,email,mobile,address}) {
  return (
    <>
-     <div className='profile'>
-       <div>
-         <h2>resume</h2>
+   <h2 className='resume'>resume</h2>
+     <section className='profile-section'>
+       <div className='profile'>
+         <div className='profile-info'>
+           <h1>{name}</h1>
+           <p className='email'>Email-id : {email}</p>
+           <p className='mobile'>Mobile No : {mobile}</p>
+           <p className='address'>Address: {address}</p>
+         </div>
        </div>
-       <div className='profile-info'>
-         <h1>{name}</h1>
-         <p className='email'>Email-id : {email}</p>
-         <p className='mobile'>Mobile No : {mobile}</p>
-         <p className='address'>Address: {address}</p>
+       <div className='profile-image'>
+         <img
+           src='https://img.freepik.com/premium-vector/man-avatar-profile-picture-vector-illustration_268834-538.jpg'
+           alt=''
+         />
        </div>
-     </div>
-     <div className='profile-image'>
-       <img src='' alt='' />
-     </div>
+     </section>
    </>
  );
 }
