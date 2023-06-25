fdc61b135ffe5567eba1c5feab332a3809f0b37f
chhotu marandi <cmarandi29@gmail.com>
2023-06-25 13:19:09 +0530

    pass props through spread operator

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import './App.css';
 import Resume from './components/Resume'
 
 function App() {
+
+  const person = {
+      name: 'ram kumar',
+      email: 'ram@gmail.com',
+      mobile: '854691356',
+      address: 'bakudih , sahibganj, jharkhand, pin - 816101'
+  }
+
   return (
-    <div className="App">
-     <Resume 
-      name='ram kumar'
-      email='ram@gmail.com'
-      mobile='854691356'
-      address='delhi,india'
-     />
+    <div className='App'>
+      <Resume {...person} />
+      <hr />
+      <hr />
     </div>
   );
 }
