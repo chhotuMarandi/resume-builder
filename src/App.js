c2c7ba816921d388268b30e3250242cf14da4ceb
chhotu marandi <cmarandi29@gmail.com>
2023-06-29 14:32:51 +0530

    pass props in personnal details section

--- src/App.js
+++ src/App.js
@@ -4,11 +4,32 @@ import Resume from './components/Resume'
 function App() {
 
   const person = {
-      name: 'ram kumar',
-      email: 'ram@gmail.com',
-      mobile: '854691356',
-      address: 'bakudih , sahibganj, jharkhand, pin - 816101'
-  }
+    name: 'ram kumar',
+    email: 'ram@gmail.com',
+    mobile: '854691356',
+    address: 'bakudih , sahibganj, jharkhand, pin - 816101',
+    skills: ['HTMl', 'CSS', 'Javascript'],
+    school: {
+      name: 'BHJP High School Bakudih',
+      percentage: '89%',
+      passingYear: '2014',
+    },
+    college: {
+      name: 'Mahadeo Singh College Bhagalpur',
+      percentage: '53%',
+      passingYear: '2016',
+    },
+    personnalDetails: {
+      fatherName: 'Bhado Marandi',
+      dob: '02/02/1999',
+      gender: 'male',
+      nationality: 'indian',
+      martialStatus: 'unmarried',
+      language: 'Hindi,English',
+      hobbies: 'reading books'
+    }
+    
+  };
 
   return (
     <div className='App'>
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,136 +1,142 @@
 import './Resume.css'
 
-function Resume ({name,email,mobile,address}) {
-
-
- return (
-   <div className='resume'>
-     <h2 className='resume-heading'>resume</h2>
-     <section className='profile-section'>
-       <div className='profile-info'>
-         <h1>{name}</h1>
-         <div className='email'>
-           <label>email-id : </label>
-           {email}
-         </div>
-         <div className='mobile'>
-           <label>mobile no :</label> {mobile}
-         </div>
-         <div className='email'>
-           <label>address : </label>
-           {address}
-         </div>
-       </div>
-       <div className='profile-image'>
-         <img
-           src='https://img.freepik.com/premium-vector/man-avatar-profile-picture-vector-illustration_268834-538.jpg'
-           alt=''
-         />
-       </div>
-     </section>
-     <hr />
-     <hr />
-     <section className='profile-info'>
-       <div className='career'>
-         <h3>career objective</h3>
-         <ul>
-           <li>
-             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat
-             voluptates vero molestias? Vitae, minus qui assumenda fugiat saepe
-             at eveniet.
-           </li>
-         </ul>
-       </div>
-       <div className='skills'>
-         <h3>skills</h3>
-         <ul className='skills-grid'>
-           <li>HTML</li>
-           <li>Node Js</li>
-           <li>CSS</li>
-           <li>Express</li>
-           <li>JavaScript</li>
-           <li>MongoDB</li>
-           <li>React Js</li>
-           <li>Git Command</li>
-         </ul>
-       </div>
-       <div className='qualification'>
-         <h3>qualification</h3>
-         <table>
-           <tr>
-             <th>university</th>
-             <th>College</th>
-             <th>Percentage</th>
-             <th>Passing Year</th>
-           </tr>
-           <tr>
-             <th>school</th>
-             <td>BHJP high school bakudi</td>
-             <td>86%</td>
-             <td>2014</td>
-           </tr>
-           <tr>
-             <th>college</th>
-             <td>Ms college Bhagalpur</td>
-             <td>59%</td>
-             <td>2016</td>
-           </tr>
-         </table>
-       </div>
-       <div className='experience'>
-         <h3>experience</h3>
-         <ul>
-           <li>
-             Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam,
-             nesciunt.
-           </li>
-           <li>
-             Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam,
-             nesciunt.
-           </li>
-         </ul>
-       </div>
-       <div className='personnal-details'>
-         <h3>personnal details</h3>
-         <div className='details'>
-           <p htmlFor=''>Father Name</p>
-           <span>:</span>
-           <p>ram kumar</p>
-         </div>
-         <div className='details'>
-           <p htmlFor=''>Date of Birth</p>
-           <span>:</span>
-           <p>05/02/1999</p>
-         </div>
-         <div className='details'>
-           <p htmlFor=''>Gender</p>
-           <span>:</span>
-           <p>Male</p>
-         </div>
-         <div className='details'>
-           <p htmlFor=''>Nationality</p>
-           <span>:</span>
-           <p>indian</p>
-         </div>
-         <div className='details'>
-           <p htmlFor=''>Martial status</p>
-           <span>:</span>
-           <p>unmarried</p>
-         </div>
-         <div className='details'>
-           <p htmlFor=''>Language known</p>
-           <span>:</span>
-           <p>Hindi, English</p>
-         </div>
-         <div className='details'>
-           <p htmlFor=''>Hobbies</p>
-           <span>:</span>
-           <p>Reading Books</p>
-         </div>
-       </div>
-     </section>
-   </div>
- );
+function Resume({
+  name,
+  email,
+  mobile,
+  address,
+  school,
+  college,
+  personnalDetails,
+}) {
+  return (
+    <div className='resume'>
+      <h2 className='resume-heading'>resume</h2>
+      <section className='profile-section'>
+        <div className='profile-info'>
+          <h1>{name}</h1>
+          <div className='email'>
+            <label>email-id : </label>
+            {email}
+          </div>
+          <div className='mobile'>
+            <label>mobile no :</label> {mobile}
+          </div>
+          <div className='email'>
+            <label>address : </label>
+            {address}
+          </div>
+        </div>
+        <div className='profile-image'>
+          <img
+            src='https://img.freepik.com/premium-vector/man-avatar-profile-picture-vector-illustration_268834-538.jpg'
+            alt=''
+          />
+        </div>
+      </section>
+      <hr />
+      <hr />
+      <section className='profile-info'>
+        <div className='career'>
+          <h3>career objective</h3>
+          <ul>
+            <li>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat
+              voluptates vero molestias? Vitae, minus qui assumenda fugiat saepe
+              at eveniet.
+            </li>
+          </ul>
+        </div>
+        <div className='skills'>
+          <h3>skills</h3>
+          <ul className='skills-grid'>
+            <li>HTML</li>
+            <li>Node Js</li>
+            <li>CSS</li>
+            <li>Express</li>
+            <li>JavaScript</li>
+            <li>MongoDB</li>
+            <li>React Js</li>
+            <li>Git Command</li>
+          </ul>
+        </div>
+        <div className='qualification'>
+          <h3>qualification</h3>
+          <table>
+            <tr>
+              <th>university</th>
+              <th>Name</th>
+              <th>Percentage</th>
+              <th>Passing Year</th>
+            </tr>
+            <tr>
+              <th>School</th>
+              <td>{school.name}</td>
+              <td>{school.percentage}</td>
+              <td>{school.passingYear}</td>
+            </tr>
+            <tr>
+              <th>College</th>
+              <td>{college.name}</td>
+              <td>{college.percentage}</td>
+              <td>{college.passingYear}</td>
+            </tr>
+          </table>
+        </div>
+        <div className='experience'>
+          <h3>experience</h3>
+          <ul>
+            <li>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam,
+              nesciunt.
+            </li>
+            <li>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam,
+              nesciunt.
+            </li>
+          </ul>
+        </div>
+        <div className='personnal-details'>
+          <h3>personnal details</h3>
+          <div className='details'>
+            <p>Father Name</p>
+            <span>:</span>
+            <p>{personnalDetails.fatherName}</p>
+          </div>
+          <div className='details'>
+            <p>Date of Birth</p>
+            <span>:</span>
+            <p>{personnalDetails.dob}</p>
+          </div>
+          <div className='details'>
+            <p>Gender</p>
+            <span>:</span>
+            <p>{personnalDetails.gender}</p>
+          </div>
+          <div className='details'>
+            <p>Nationality</p>
+            <span>:</span>
+            <p>{personnalDetails.nationality}</p>
+          </div>
+          <div className='details'>
+            <p>Martial status</p>
+            <span>:</span>
+            <p>{personnalDetails.martialStatus}</p>
+          </div>
+          <div className='details'>
+            <p>Language known</p>
+            <span>:</span>
+            <p>{personnalDetails.language}</p>
+          </div>
+          <div className='details'>
+            <p>Hobbies</p>
+            <span>:</span>
+            <p>{personnalDetails.hobbies}</p>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
 
 export default Resume
